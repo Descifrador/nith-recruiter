f0df54325ef4fd4d8ee44e454b93c651d28a3788
anand9git <aggarwalanand9@gmail.com>
2021-12-11 15:28:14 +0530

    added firebase

--- src/App.js
+++ src/App.js
@@ -3,6 +3,25 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Links from "./Routes";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBW6Do0Qg9Gbk3Ajbsr1zEGoDCaK7JC_Qs",
+  authDomain: "nith-recruiter.firebaseapp.com",
+  projectId: "nith-recruiter",
+  storageBucket: "nith-recruiter.appspot.com",
+  messagingSenderId: "998833580900",
+  appId: "1:998833580900:web:e822e4a667c256da646871"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 function App() {  
   return (
     <div className="App">
